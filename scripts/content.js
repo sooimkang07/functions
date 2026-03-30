6caf33c47f426f53eb3f4f433bfdcdc880163857
sooimkang <98446051+sooimkang@users.noreply.github.com>
2026-03-30 13:22:50 -0400

    MATCH PATTERNS
    COMPLETED:
    - changed matches in json to <all_urls> to now run on every page

--- scripts/content.js
+++ scripts/content.js
@@ -1,35 +1,37 @@
-function renderReadingTime(article) {
-  // If we weren't provided an article, we don't need to render anything.
-  if (!article) {
-	return;
-  }
+// function renderReadingTime(article) {
+//   // If we weren't provided an article, we don't need to render anything.
+//   if (!article) {
+// 	return;
+//   }
 
-  // get all text in article
-  const text = article.textContent;
+//   // get all text in article
+//   const text = article.textContent;
 
-  // divied up text into words
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
+//   // divied up text into words
+//   const wordMatchRegExp = /[^\s]+/g; // Regular expression
+//   const words = text.matchAll(wordMatchRegExp);
 
-  // matchAll returns an iterator, convert to array to get word count
-  // get length of array aka word count in article
-  const wordCount = [...words].length;
+//   // matchAll returns an iterator, convert to array to get word count
+//   // get length of array aka word count in article
+//   const wordCount = [...words].length;
 
-  // dividing total word count by average reading time of 200 words per min and rounding to nearest whole number to get reading time
-  const readingTime = Math.round(wordCount / 200);
+//   // dividing total word count by average reading time of 200 words per min and rounding to nearest whole number to get reading time
+//   const readingTime = Math.round(wordCount / 200);
 
-  // create a badge element to show reading time info
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
+//   // create a badge element to show reading time info
+//   const badge = document.createElement("p");
+//   // Use the same styling as the publish information in an article's header
+//   badge.classList.add("color-secondary-text", "type--caption");
+//   badge.textContent = `⏱️ ${readingTime} min read`;
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
+//   // Support for API reference docs
+//   const heading = article.querySelector("h1");
+//   // Support for article docs with date
+//   const date = article.querySelector("time")?.parentNode;
 
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-}
+//   (date ?? heading).insertAdjacentElement("afterend", badge);
+// }
 
-renderReadingTime(document.querySelector("article"));
+// renderReadingTime(document.querySelector("article"));
+
+console.log("is this on?")
