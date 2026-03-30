432e8399d22bda20b20e34f5a332fd50b9314865
sooimkang <98446051+sooimkang@users.noreply.github.com>
2026-03-30 14:29:50 -0400

    MAKE ACTIVE TABS
    COMPLETED:
    - "Make Active" button now takes you to the tab in the popup
    - commented out breakdown of all functions in popup.js file from Eric

--- tabs-manager/popup.js
+++ tabs-manager/popup.js
@@ -3,7 +3,7 @@
 const tabs = await chrome.tabs.query({});
 
 // giving back the list of chrome tabs open
-// console.log(tabs)
+console.log(tabs)
 
 let ul = document.querySelector('ul')
 
@@ -12,7 +12,22 @@ tabs.forEach((tab) => {
     <li>
         <h1>${tab.title}</h1>
         <p>${tab.url}</p>
+        // data attribute can add to any html element and pass in data without necessarily modifying anything else. always starts with data- and then whatever you want to call it. 
+        <button data-tab-id="${tab.id}">Make Active</button>
     </li>
     `
     ul.insertAdjacentHTML('beforeend', content)
+})
+
+// looking for only all my ul buttons inside my ul instead of in the whole page
+const buttons = document.querySelectorAll('ul button')
+
+// each button click gets the tab id from chrome's tab API
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // parseInt turns the string into number which is what chrome needs. 
+        // dataset is hs that says look for anything that starts with data attribute and the tadId is the value so converting all tab-id to tabId.
+        let tabId = parseInt(button.dataset.tabId)
+        chrome.tabs.update(tabId, {active: true})
+    })
 })
