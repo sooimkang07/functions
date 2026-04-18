f821eff3fc124f29498cd59ea325b73dec618fff
sooimkang <98446051+sooimkang@users.noreply.github.com>
2026-04-18 17:17:54 -0400

    CHANGED ARROW SVG

--- scripts/popup.js
+++ scripts/popup.js
@@ -74,8 +74,7 @@ const createPageItem = (page) => {
 				</button>
 				<button class="popup-page-toggle" type="button" aria-label="Toggle annotations">
 					<span class="popup-page-count">(${count})</span>
-					<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-				</button>
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z" fill="currentColor"/></svg>				</button>
 			</div>
 			<ul class="popup-annotation-list" hidden>
 				${annotationItems}
